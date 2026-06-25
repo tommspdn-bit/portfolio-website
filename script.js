558f77c4fa5c44bc1c430d2187fa3fb7ee33adc4
Tom <tomspdnn@gmail.com>
2026-06-25 17:39:43 +0100

    Lightbox changes

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
+// Hero slideshow
 const slides = document.querySelectorAll(".hero-slide");
 
-let currentSlide = 0;
+if (slides.length > 0) {
+  let currentSlide = 0;
 
-setInterval(() => {
+  setInterval(() => {
     slides[currentSlide].classList.remove("active");
 
     currentSlide = (currentSlide + 1) % slides.length;
 
     slides[currentSlide].classList.add("active");
-}, 4000);
+  }, 4000);
+}
+
+// Gallery lightbox
+const galleryImages = document.querySelectorAll(".project-gallery img");
+const lightbox = document.getElementById("lightbox");
+const lightboxImage = document.querySelector(".lightbox img");
+
+if (galleryImages.length > 0 && lightbox && lightboxImage) {
+  galleryImages.forEach((image) => {
+    image.addEventListener("click", () => {
+      lightboxImage.src = image.src;
+      lightbox.classList.add("active");
+    });
+  });
+
+  lightbox.addEventListener("click", () => {
+    lightbox.classList.remove("active");
+    lightboxImage.src = "";
+  });
+}
